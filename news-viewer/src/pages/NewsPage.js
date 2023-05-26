1e27328ab9e9134fc3de79fd537244b3cc2a1bc2
lsy3709 <lsy3709@naver.com>
2023-05-26 13:46:38 +0900

    first commit

---
+++ news-viewer/src/pages/NewsPage.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Categories from '../components/Categories';
+import NewsList from '../components/NewsList';
+
+const NewsPage = ({ match }) => {
+  const params = useParams();
+  // 카테고리가 선택되지 않았으면 기본값 all로 사용
+  const category = params.category || 'all';
+
+  useEffect( () => {
+      const titleElement = document.getElementsByTagName("title")[0];
+      titleElement.innerHTML=`뉴스 -${params.category}`;
+    },[params.category ] );
+
+  return (
+    <>
+      <Categories />
+      <NewsList category={category} />
+    </>
+  );
+};
+
+export default NewsPage;
